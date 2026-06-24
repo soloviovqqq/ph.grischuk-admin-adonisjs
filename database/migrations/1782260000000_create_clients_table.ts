d8cfc12a44bd1b625b1a4c3e48b33e88ab39a797
sqqq <soloviovqqq@gmail.com>
2026-06-24 22:05:27 +0200

    wip

--- database/migrations/1782260000000_create_clients_table.ts
+++ database/migrations/1782260000000_create_clients_table.ts
@@ -7,13 +7,12 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
       table.string('name').notNullable()
-      table.string('address_line_1').notNullable()
-      table.string('address_line_2').nullable()
+      table.string('address').notNullable()
       table.string('zip').notNullable()
       table.string('city').notNullable()
       table.string('country').notNullable()
-      table.string('email', 254).nullable()
-
+      table.string('phone').nullable()
+      table.string('email').nullable()
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
     })
